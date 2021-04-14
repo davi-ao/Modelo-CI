6137c843f60b93879943913ac5083f7c027d987f
Davi Alves Oliveira <davi.alvesoliveira@gmail.com>
2021-04-13 22:27:20 -0300

    Grafos gerados

--- Simulação - Aborto.R
+++ Simulação - Aborto.R
@@ -11,21 +11,27 @@
 # P4 PORQUE [P3, P5]                P12 MODIFICA [POSSIBILIDADE, P13]
 # P5 SE [P6, P7]                    P13 INTERFERIR [GOVERNO, P14]
 # P6 DAR [DEUS, VIDA]               P14 MODIFICA [PESSOAIS, DECISÕES]
-# P7 MODIFICA [POSSIBILIDADE, P8]   P8 TIRAR [DEUS, VIDA]
+# P7 MODIFICA [POSSIBILIDADE, P8]   
+# P8 TIRAR [DEUS, VIDA]
+
+# Leitor A: contra a descriminalização do aborto, P1 a P8 mais forte
+# Leitor B: a favor da descriminalização do aborto, P9 a P14 mais forte
 
 library(tidyverse)
 library(igraph)
 
-C = matrix(c(
+# Leitor A: contra a descriminalização
+
+C_A = matrix(c(
   #P1  P2  P3  P4  P5  P6  P7  P8  P9  P10 P11 P12 P13 P14
-   1,  1,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0, #P1
-   1,  1,  1,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0, #P2
-   0,  1,  1,  1,  0,  0,  0,  0, -1,  0,  0,  0,  0,  0, #P3
-   0,  0,  1,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0, #P4
-   0,  0,  0,  1,  1,  1,  1,  0,  0,  0,  0,  0,  0,  0, #P5
-   0,  0,  0,  0,  1,  1,  0,  1,  0,  0,  0,  0,  0,  0, #P6
-   0,  0,  0,  0,  1,  0,  1,  1,  0,  0,  0,  1,  0,  0, #P7
-   0,  0,  0,  0,  0,  1,  1,  1,  0,  0,  0,  0,  0,  0, #P8
+ 1.5,1.5,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0, #P1
+ 1.5,1.5,1.5,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0, #P2
+   0,1.5,1.5,1.5,  0,  0,  0,  0, -1,  0,  0,  0,  0,  0, #P3
+   0,  0,1.5,1.5,1.5,  0,  0,  0,  0,  0,  0,  0,  0,  0, #P4
+   0,  0,  0,1.5,1.5,1.5,1.5,  0,  0,  0,  0,  0,  0,  0, #P5
+   0,  0,  0,  0,1.5,1.5,  0,1.5,  0,  0,  0,  0,  0,  0, #P6
+   0,  0,  0,  0,1.5,  0,1.5,1.5,  0,  0,  0,  1,  0,  0, #P7
+   0,  0,  0,  0,  0,1.5,1.5,1.5,  0,  0,  0,  0,  0,  0, #P8
    1,  1, -1,  0,  0,  0,  0,  0,  1,  1,  0,  0,  0,  0, #P9
    0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  0,  0,  0, #P10
    0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  0,  0, #P11
@@ -34,137 +40,164 @@ C = matrix(c(
    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1  #P14
    ), nrow = 14) # Conexões
 
-colnames(C) = paste("P", 1:14, sep = "") # Renomear colunas
-rownames(C) = colnames(C) # Renomear linhas
+colnames(C_A) = paste("P", 1:14, sep = "") # Renomear colunas
+rownames(C_A) = colnames(C_A) # Renomear linhas
 
 # Conferir se matriz é simétrica
-all(C == t(C))
+all(C_A == t(C_A))
 
-# PRIMEIRO CICLO - Leitor A: contra o aborto
+# PRIMEIRO CICLO
 A1 = matrix(c(1, 1, 1, 1, 1, 1, 1, 1), nrow = 1) # Ativação inicial do ciclo 1
-colnames(A1) = colnames(C[,1:length(A1)]) # Renomear linhas
+colnames(A1) = colnames(C_A[,1:length(A1)]) # Renomear linhas
 
 t = 1
 while(
   any(abs(
-    A1 - (A1 %*% C[1:length(A1),1:length(A1)])/
-    max(A1 %*% C[1:length(A1),1:length(A1)])) > .001
+    A1 - (A1 %*% C_A[1:length(A1),1:length(A1)])/
+    max(A1 %*% C_A[1:length(A1),1:length(A1)])) > .001
   )) {
   A1 = (
-    A1 %*% C[1:length(A1),1:length(A1)])/
-    max(A1 %*% C[1:length(A1),1:length(A1)]) # Estabilização (t = 23 iterações)
+    A1 %*% C_A[1:length(A1),1:length(A1)])/
+    max(A1 %*% C_A[1:length(A1),1:length(A1)]) # Estabilização (t = 23 iterações)
   t = t + 1
 }
 
-# SEGUNDO CICLO  - Leitor A: contra o aborto
-A2 = matrix(c(A1, -1, 1, 1, 1, 1, 1), nrow = 1) # Ativação inicial do ciclo 2
-colnames(A2) = colnames(C[,1:length(A2)]) # Renomear colunas
+# SEGUNDO CICLO
+A2 = matrix(c(A1, 1, 1, 1, 1, 1, 1), nrow = 1) # Ativação inicial do ciclo 2
+colnames(A2) = colnames(C_A[,1:length(A2)]) # Renomear colunas
 
 t = 1
 while(
   any(abs(
-    A2 - (A2 %*% C[1:length(A2),1:length(A2)])/
-    max(A2 %*% C[1:length(A2),1:length(A2)])) > .001
+    A2 - (A2 %*% C_A[1:length(A2),1:length(A2)])/
+    max(A2 %*% C_A[1:length(A2),1:length(A2)])) > .001
   )) {
   A2 = (
-    A2 %*% C[1:length(A2),1:length(A2)])/
-    max(A2 %*% C[1:length(A2),1:length(A2)]) # Estabilização (t = 21 iterações)
+    A2 %*% C_A[1:length(A2),1:length(A2)])/
+    max(A2 %*% C_A[1:length(A2),1:length(A2)]) # Estabilização (t = 21 iterações)
   t = t + 1
 }
 
-# Memória de Longo Prazo - Leitor A: contra o aborto
+# Memória de Longo Prazo - Leitor A
 M_A = matrix(rep(rep(0, ncol(A2)), ncol(A2)), nrow = ncol(A2))
 
-for (i in 1:nrow(C)) {
-  for (j in 1:ncol(C))
-    M_A[i, j] = ifelse(i <= nrow(C) & j <= ncol(C), (C[i, j] * A2[i] * A2[j]), 0)
+for (i in 1:nrow(C_A)) {
+  for (j in 1:ncol(C_A))
+    M_A[i, j] = ifelse(i <= nrow(C_A) & j <= ncol(C_A), (C_A[i, j] * A2[i] * A2[j]), 0)
 }
 
-colnames(M_A) = colnames(C) # Renomear colunas
-rownames(M_A) = colnames(C) # Renomear linhas
+colnames(M_A) = colnames(C_A) # Renomear colunas
+rownames(M_A) = colnames(C_A) # Renomear linhas
 
 # -----------------------------------------------------------------------------
 
-# PRIMEIRO CICLO - Leitor B: a favor do aborto
-B1 = matrix(c(1, 1, -1, 1, 1, 1, 1, 1), nrow = 1) # Ativação inicial do ciclo 1
-colnames(B1) = colnames(C[,1:length(B1)]) # Renomear linhas
+# Leitor B: a favor da descriminalização aborto
+
+C_B = matrix(c(
+  #P1 P2   P3  P4  P5  P6  P7  P8   P9  P10 P11 P12 P13 P14
+  1,  1,   0,  0,  0,  0,  0,  0, 1.5,  0,  0,  0,  0,  0, #P1
+  1,  1,   1,  0,  0,  0,  0,  0, 1.5,  0,  0,  0,  0,  0, #P2
+  0,  1,   1,  1,  0,  0,  0,  0,-1.5,  0,  0,  0,  0,  0, #P3
+  0,  0,   1,  1,  1,  0,  0,  0,   0,  0,  0,  0,  0,  0, #P4
+  0,  0,   0,  1,  1,  1,  1,  0,   0,  0,  0,  0,  0,  0, #P5
+  0,  0,   0,  0,  1,  1,  0,  1,   0,  0,  0,  0,  0,  0, #P6
+  0,  0,   0,  0,  1,  0,  1,  1,   0,  0,  0,1.5,  0,  0, #P7
+  0,  0,   0,  0,  0,  1,  1,  1,   0,  0,  0,  0,  0,  0, #P8
+1.5,1.5,-1.5,  0,  0,  0,  0,  0, 1.5,1.5,  0,  0,  0,  0, #P9
+  0,  0,   0,  0,  0,  0,  0,  0, 1.5,1.5,1.5,  0,  0,  0, #P10
+  0,  0,   0,  0,  0,  0,  0,  0,   0,1.5,1.5,1.5,  0,  0, #P11
+  0,  0,   0,  0,  0,  0,1.5,  0,   0,  0,1.5,1.5,1.5,  0, #P12
+  0,  0,   0,  0,  0,  0,  0,  0,   0,  0,  0,1.5,1.5,1.5, #P13
+  0,  0,   0,  0,  0,  0,  0,  0,   0,  0,  0,  0,1.5,1.5  #P14
+), nrow = 14) # Conexões
+
+colnames(C_B) = paste("P", 1:14, sep = "") # Renomear colunas
+rownames(C_B) = colnames(C_B) # Renomear linhas
+
+# Conferir se matriz é simétrica
+all(C_B == t(C_B))
+
+
+# PRIMEIRO CICLO - Leitor B
+B1 = matrix(c(1, 1, 1, 1, 1, 1, 1, 1), nrow = 1) # Ativação inicial do ciclo 1
+colnames(B1) = colnames(C_B[,1:length(B1)]) # Renomear linhas
 
 t = 1
 while(
   any(abs(
-    B1 - (B1 %*% C[1:length(B1),1:length(B1)])/
-    max(B1 %*% C[1:length(B1),1:length(B1)])) > .001
+    B1 - (B1 %*% C_B[1:length(B1),1:length(B1)])/
+    max(B1 %*% C_B[1:length(B1),1:length(B1)])) > .001
   )) {
   B1 = (
-    B1 %*% C[1:length(B1),1:length(B1)])/
-    max(B1 %*% C[1:length(B1),1:length(B1)]) # Estabilização (t = 23 iterações)
+    B1 %*% C_B[1:length(B1),1:length(B1)])/
+    max(B1 %*% C_B[1:length(B1),1:length(B1)]) # Estabilização (t = 23 iterações)
   t = t + 1
 }
 
-# SEGUNDO CICLO  - Leitor A: contra o aborto
+# SEGUNDO CICLO  - Leitor B
 B2 = matrix(c(B1, 1, 1, 1, 1, 1, 1), nrow = 1) # Ativação inicial do ciclo 2
-colnames(B2) = colnames(C[,1:length(B2)]) # Renomear colunas
+colnames(B2) = colnames(C_B[,1:length(B2)]) # Renomear colunas
 
 t = 1
 while(
   any(abs(
-    B2 - (B2 %*% C[1:length(B2),1:length(B2)])/
-    max(B2 %*% C[1:length(B2),1:length(B2)])) > .001
+    B2 - (B2 %*% C_B[1:length(B2),1:length(B2)])/
+    max(B2 %*% C_B[1:length(B2),1:length(B2)])) > .001
   )) {
   B2 = (
-    B2 %*% C[1:length(B2),1:length(B2)])/
-    max(B2 %*% C[1:length(B2),1:length(B2)]) # Estabilização (t = 21 iterações)
+    B2 %*% C_B[1:length(B2),1:length(B2)])/
+    max(B2 %*% C_B[1:length(B2),1:length(B2)]) # Estabilização (t = 21 iterações)
   t = t + 1
 }
 
-# Memória de Longo Prazo - Leitor A: contra o aborto
+# Memória de Longo Prazo - Leitor B
 M_B = matrix(rep(rep(0, ncol(B2)), ncol(B2)), nrow = ncol(B2))
 
-for (i in 1:nrow(C)) {
-  for (j in 1:ncol(C))
-    M_B[i, j] = ifelse(i <= nrow(C) & j <= ncol(C), (C[i, j] * B2[i] * B2[j]), 0)
+for (i in 1:nrow(C_B)) {
+  for (j in 1:ncol(C_B))
+    M_B[i, j] = ifelse(i <= nrow(C_B) & j <= ncol(C_B), (C_B[i, j] * B2[i] * B2[j]), 0)
 }
 
-colnames(M_B) = colnames(C) # Renomear colunas
-rownames(M_B) = colnames(C) # Renomear linhas
+colnames(M_B) = colnames(C_B) # Renomear colunas
+rownames(M_B) = colnames(C_B) # Renomear linhas
 
 # ------------------------------------------------------------------------------
 
 # Grafos
 # Referência: https://kateto.net/network-visualization
 
-# Leitor A
+# Leitor A: contra a descriminalização
 G_A = graph_from_adjacency_matrix(M_A, weighted = T) %>%
   simplify(remove.multiple = F, remove.loops = T)
 
-deg = G_A %>% degree(mode = 'all')
+V(G_A)$size = sqrt(pi * strength(G_A)) * 5
+E(G_A)$width = sqrt(E(G_A)$weight) * 7
 
-V(G_A)$size = deg * 5
-E(G_A)$width = E(G_A)$weight * 5
-
-graph_attr(G_A, "layout") = layout_with_lgl
+graph_attr(G_A, "layout") = layout_with_lgl(G_A, root = "P5")
 
 plot(G_A, 
      edge.arrow.size = 0,
      vertex.color = '#FF968A',
      vertex.label.family = 'sans',
      vertex.label.color = '#333333',
-     vertex.frame.color = '#FFFFFF')
+     vertex.frame.color = '#FFFFFF',
+     main = 'Leitor A',
+     submain = '(contra a descriminalização)')
 
-# Leitor B
+# Leitor B: a favor da descriminalização
 G_B = graph_from_adjacency_matrix(M_B, weighted = T) %>%
   simplify(remove.multiple = F, remove.loops = T)
 
-deg = G_B %>% degree(mode = 'all')
+V(G_B)$size = sqrt(pi * strength(G_B)) * 5
+E(G_B)$width = sqrt(E(G_B)$weight) * 7
 
-V(G_B)$size = deg * 5
-E(G_B)$width = E(G_B)$weight * 5
-
-graph_attr(G_B, "layout") = layout_with_lgl
+graph_attr(G_B, "layout") = layout_with_lgl(G_B, root = "P9")
 
 plot(G_B, 
      edge.arrow.size = 0,
      vertex.color = '#FF968A',
      vertex.label.family = 'sans',
      vertex.label.color = '#333333',
-     vertex.frame.color = '#FFFFFF')
+     vertex.frame.color = '#FFFFFF',
+     main = 'Leitor B',
+     submain = '(a favor da descriminalização)')
